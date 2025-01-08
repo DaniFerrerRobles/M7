b691bb5baf1c7999f053ddb1e563d172ba06fd95
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-01-08 17:45:34 +0000

    .

--- PracticasUF2/proyecto0/biblioteca.php
+++ PracticasUF2/proyecto0/biblioteca.php
@@ -4,17 +4,26 @@ class Biblioteca {
     public $llibres = [];
 
     public function afegirLlibre($llibre) {
-        $this->llibres[] = $llibre;
-    }
+        array_push($this->llibres, $llibre);
+    }    
 
     public function mostrarLlibres() {
         $detallsLlibres = [];
         foreach ($this->llibres as $llibre) {
             $detallsLlibres[] = $llibre->detallsLlibre();
         }
-
-        return implode("<br>", $detallsLlibres);
+        return $detallsLlibres;
     }
 
+    public function cercarLlibrePelTitol($text) {
+        $resultats = [];
+        foreach ($this->llibres as $llibre) {
+            if (stripos($llibre->titol, $text) == true) {
+                $resultats[] = $llibre->detallsLlibre();
+            }
+        }
+        return $resultats;
+    }
 }
+
 ?>
