75203d0912197b31fd2ecd8182a51ace809e4d19
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-01-15 18:01:21 +0000

    .

--- PracticasUF2/proyecto2/baraja.class.php
+++ PracticasUF2/proyecto2/baraja.class.php
@@ -4,13 +4,13 @@ class Baraja {
     public $conjunto_cartas = [];
 
     public function crea_baraja() {
-        $coloresCartas = ['amarillo', 'rojo', 'verde', 'azul', 'reverse', 'skip', 'picker'];
-        $numerosCartas = [(range(1, 9))];
+        $coloroTipoCartas = ['amarillo', 'rojo', 'verde', 'azul', 'reverse', 'skip', 'picker'];
+        $numerosCartas = range(1, 9);
 
-        $identificador = 0;
-        foreach ($coloresCartas as $colorCarta) {
+        $identificadorCarta = 0;
+        foreach ($coloroTipoCartas as $coloroTipoCarta) {
             foreach ($numerosCartas as $numeroCarta) {
-                $this->conjunto_cartas[] = new Carta($colorCarta, $numeroCarta, $identificador++);
+                $this->conjunto_cartas[] = new Carta($coloroTipoCarta, $numeroCarta, $identificadorCarta++);
             }
         }
     }
@@ -21,13 +21,13 @@ class Baraja {
 
     public function pinta_baraja() {
         foreach ($this->conjunto_cartas as $carta) {
-            $carta->pinta_carta();
+           $carta->pinta_carta();
         }
     }
 
     public function pinta_baraja_girada() {
         foreach ($this->conjunto_cartas as $carta) {
-            $carta->pinta_carta_girada();
+           $carta->pinta_carta_girada();
         }
     }
 }
