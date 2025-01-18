9676da443199a3dbd817bcb5af8dba114eb1fc8c
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-01-18 20:54:23 +0000

    .

--- PracticasUF2/proyecto2/index.php
+++ PracticasUF2/proyecto2/index.php
@@ -26,11 +26,11 @@ $cartaInicial = array_shift($baraja->conjunto_cartas);
 echo "<p>Carta sobre la mesa: <img src='cartas_uno/" . $cartaInicial->numeroCarta . "_" 
     . $cartaInicial->coloroTipoCarta . ".png' alt='Carta inicial' /></p>";
 
-    foreach ($jugadores as $jugador => $manoDelJugador) {
+    foreach ($jugadores as $jugador => $cartasDeJugadores) {
         echo "<h3>Jugador " . ($jugador) . ":</h3>";
         
-        foreach ($manoDelJugador as $carta) {
-            $carta->pinta_carta();
+        foreach ($cartasDeJugadores as $cartaDeJugador) {
+            $cartaDeJugador->pinta_carta();
         }
     }    
 ?>
