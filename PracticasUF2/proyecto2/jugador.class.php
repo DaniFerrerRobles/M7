0da189184640325b6fc2bca5ddf9c4ca876a4be1
DaniFerrerRobles <120500268+DaniFerrerRobles@users.noreply.github.com>
2025-01-22 19:56:52 +0000

    .

--- PracticasUF2/proyecto2/jugador.class.php
+++ PracticasUF2/proyecto2/jugador.class.php
@@ -1,15 +1,27 @@
 <?php
+include 'carta.class.php';
 
 class Jugador {
-    public $mano, $id;
-
-    public function __construct($id) {
-        $this->id = $id;
-        $this->mano = [];
-    }
-
+    public $mano = [];
+    public $id;
 
     public function afegir_carta($carta) {
-        $this->mano[] = $carta;
+        $mano[] = $carta;
+    }
+
+    public function eliminar_carta($carta) {
+        foreach ($mano as $cartas => $cartaActual) {
+            array_shift($carta);
+            }
+        }
+    }
+
+    public function mostrar_ma() {
+        foreach ($jugadores as $jugador => $cartas) {
+
+            foreach ($cartas as $carta) {
+                $carta->pinta_carta();
+            }
     }
 }
+?>
